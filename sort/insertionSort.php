f37425d4145a9e868b12cb049128c8a97229a5b6
Maksim Fomenok <maksim.fomenok@gmail.com>
2018-05-30 01:23:36 +0300

    Implement insertionSort in PHP

---
+++ sort/insertionSort.php
@@ -0,0 +1,19 @@
+<?php
+
+function insertionSort(array $array): array
+{
+    $length = count($array);
+
+    for ($i = 1; $i < $length; ++$i) {
+        for ($j = $i; $j > 0; --$j) {
+            if ($array[$j] < $array[$j - 1]) {
+                $tmp = $array[$j];
+                $array[$j] = $array[$j - 1];
+                $array[$j - 1] = $tmp;
+            }
+        }
+    }
+    return $array;
+}
+
+print_r(insertionSort([94, 5, 82, 9, 1, 104]));
